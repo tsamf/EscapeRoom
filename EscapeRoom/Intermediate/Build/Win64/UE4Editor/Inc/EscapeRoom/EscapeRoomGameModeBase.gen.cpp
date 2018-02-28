db971869bc8c7f845733ff498bedb049d7ed4f98
DESKTOP-NLDOG7Q\lol <samfordt@gmail.com>
2018-02-27 21:50:48 -0600

    Adding the project to git

---
+++ EscapeRoom/Intermediate/Build/Win64/UE4Editor/Inc/EscapeRoom/EscapeRoomGameModeBase.gen.cpp
@@ -0,0 +1,69 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "EscapeRoomGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeEscapeRoomGameModeBase() {}
+// Cross Module References
+	ESCAPEROOM_API UClass* Z_Construct_UClass_AEscapeRoomGameModeBase_NoRegister();
+	ESCAPEROOM_API UClass* Z_Construct_UClass_AEscapeRoomGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_EscapeRoom();
+// End Cross Module References
+	void AEscapeRoomGameModeBase::StaticRegisterNativesAEscapeRoomGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_AEscapeRoomGameModeBase_NoRegister()
+	{
+		return AEscapeRoomGameModeBase::StaticClass();
+	}
+	UClass* Z_Construct_UClass_AEscapeRoomGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+				(UObject* (*)())Z_Construct_UPackage__Script_EscapeRoom,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+				{ "IncludePath", "EscapeRoomGameModeBase.h" },
+				{ "ModuleRelativePath", "EscapeRoomGameModeBase.h" },
+				{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+			};
+#endif
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<AEscapeRoomGameModeBase>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&AEscapeRoomGameModeBase::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00900288u,
+				nullptr, 0,
+				nullptr, 0,
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AEscapeRoomGameModeBase, 2155473432);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AEscapeRoomGameModeBase(Z_Construct_UClass_AEscapeRoomGameModeBase, &AEscapeRoomGameModeBase::StaticClass, TEXT("/Script/EscapeRoom"), TEXT("AEscapeRoomGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AEscapeRoomGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ EscapeRoom/Intermediate/Build/Win64/UE4Editor/Inc/EscapeRoom/EscapeRoomGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ESCAPEROOM_EscapeRoomGameModeBase_generated_h
+#error "EscapeRoomGameModeBase.generated.h already included, missing '#pragma once' in EscapeRoomGameModeBase.h"
+#endif
+#define ESCAPEROOM_EscapeRoomGameModeBase_generated_h
+
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_RPC_WRAPPERS
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEscapeRoomGameModeBase(); \
+	friend ESCAPEROOM_API class UClass* Z_Construct_UClass_AEscapeRoomGameModeBase(); \
+public: \
+	DECLARE_CLASS(AEscapeRoomGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/EscapeRoom"), NO_API) \
+	DECLARE_SERIALIZER(AEscapeRoomGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAEscapeRoomGameModeBase(); \
+	friend ESCAPEROOM_API class UClass* Z_Construct_UClass_AEscapeRoomGameModeBase(); \
+public: \
+	DECLARE_CLASS(AEscapeRoomGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/EscapeRoom"), NO_API) \
+	DECLARE_SERIALIZER(AEscapeRoomGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEscapeRoomGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEscapeRoomGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEscapeRoomGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEscapeRoomGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEscapeRoomGameModeBase(AEscapeRoomGameModeBase&&); \
+	NO_API AEscapeRoomGameModeBase(const AEscapeRoomGameModeBase&); \
+public:
+
+
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEscapeRoomGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEscapeRoomGameModeBase(AEscapeRoomGameModeBase&&); \
+	NO_API AEscapeRoomGameModeBase(const AEscapeRoomGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEscapeRoomGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEscapeRoomGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEscapeRoomGameModeBase)
+
+
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_12_PROLOG
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_RPC_WRAPPERS \
+	EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_INCLASS \
+	EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID EscapeRoom_Source_EscapeRoom_EscapeRoomGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
